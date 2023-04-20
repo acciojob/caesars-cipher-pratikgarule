ebab32377ce31c40b0a9878dd4f7405fc93c09db
pratikgarule <127820064+pratikgarule@users.noreply.github.com>
2023-04-20 11:20:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Your Script here.
 
-var lookup = {
+const lookup = {
   'A': 'N','B': 'O','C': 'P','D': 'Q',
   'E': 'R','F': 'S','G': 'T','H': 'U',
   'I': 'V','J': 'W','K': 'X','L': 'Y',
@@ -9,25 +9,21 @@ var lookup = {
   'U': 'H','V': 'I','W': 'J','X': 'K',
   'Y': 'L','Z': 'M', '?': '?', ',': ','
 };
-
+const str="AMAN";
 function rot13(encodedStr){
-	var words = encodedStr.split(" ");
    let decodedArr = []; // Your Result goes here
   // Only change code below this line
-for (let i = 0; i < words.length; i++) {
-    const word = words[i];
-    let decoded_word = "";
-    for (let j = 0; j < word.length; j++) {
-      var char = word.charAt(j);
-      var decoded_char = lookup[char];
-      decoded_word = decoded_word + decoded_char;
-    }
-    decodedArr.push(decoded_word);
-  }
-  return decodedArr.join(" ");
+	
+	for(let i=0;i<encodedStr.length;i++){
+		decodedArr.push(lookup[encodedStr[i]]);
+	}
+
+  return decodedArr;//return decodedArr
 }
+
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 // console.log(rot13("SERR YBIR? NPPVBWBO"));
+console.log(rot13(str))
 
 module.exports = rot13;
